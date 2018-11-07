30491d3fb89293857a1da775242d85de1c7a7a5e
Jordan Vadala <jr.vadala@gmail.com>
2018-11-06 22:56:20 -0500

    Folder Updates

---
+++ Chocolatey/Choco-Application-Install.ps1
@@ -0,0 +1,10 @@
+choco install 7zip.install adobereader jre8 flashplayeractivex firefox adobeair citrix-receiver dotnet4.7.1 ie11 -y
+
+
+
+#more customized install for power users
+
+choco install 7zip.install adobereader jre8 flashplayeractivex firefox adobeair vlc ccleaner notepadplusplus putty.install filezilla teamviewer winscp virtualbox dropbox -y
+
+
+
---
+++ Git Commands/Git-File-Untrack-Fix.ps1
@@ -0,0 +1,7 @@
+git rm -r --cached .
+
+
+git add .
+
+
+git commit -m "fixed untracked files"
